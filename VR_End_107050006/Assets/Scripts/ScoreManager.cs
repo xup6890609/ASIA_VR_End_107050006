3d7b5146da98339e261e7ca079f92ea53e2ee70a
xup6890609 <xup6890609@gmail.com>
2021-01-01 22:08:46 +0800

    音效、程式大部分完成(缺黏住、不同得分區域)

---
+++ VR_End_107050006/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("得分介面")]
+    public Text textScore;
+    [Header("分數")]
+    public int score;
+    [Header("射進的分數")]
+    public int scoreIn = 50;
+
+   /// <summary>
+   /// 加音效
+   /// </summary>
+    [Header("射板音效")]
+    public AudioClip soundIn;
+    private AudioSource aud;
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// 額外加分區
+    /// </summary>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "飛鏢")
+        {
+            AddScore();
+        }
+        if (other.transform.root.name =="Player")
+        {
+            scoreIn = 500;
+
+        }
+    }
+    /// <summary>
+    /// 走出加分區
+    /// </summary>
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root.name =="Player")
+        {
+            score = 50;
+        }
+    }
+    /// <summary>
+    /// 加分
+    /// </summary>
+    private void AddScore()
+    {
+        score += scoreIn;
+        textScore.text = "分數：" + score;
+        aud.PlayOneShot(soundIn, Random.Range(0.5f, 1f));
+    }
+}
