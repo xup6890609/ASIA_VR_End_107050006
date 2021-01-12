180c175c63da71b5427f08c506c33471c7f28245
xup6890609 <71679918+xup6890609@users.noreply.github.com>
2021-01-12 12:05:03 +0800

    調整發布檔模型比例大小

---
+++ VR_End_107050006/Assets/Scripts/Dart.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Dart : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "三倍區")
+        {
+            ScoreManager.instance.AddScore(3);
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;           //把飛鏢黏在面板上
+        }
+
+        if (other.name == "兩倍區")
+        {
+            ScoreManager.instance.AddScore(2);
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;           //把飛鏢黏在面板上
+        }
+    }
+}
--- VR_End_107050006/Assets/Scripts/ScoreManager.cs
+++ VR_End_107050006/Assets/Scripts/ScoreManager.cs
@@ -6,13 +6,20 @@ public class ScoreManager : MonoBehaviour
     [Header("得分介面")]
     public Text textScore;
     [Header("分數")]
-    public int score;
+    public static int score;
     [Header("射進的分數")]
     public int scoreIn = 50;
 
-   /// <summary>
-   /// 加音效
-   /// </summary>
+    public static ScoreManager instance;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    /// <summary>
+    /// 加音效
+    /// </summary>
     [Header("射板音效")]
     public AudioClip soundIn;
     private AudioSource aud;
@@ -26,11 +33,11 @@ public class ScoreManager : MonoBehaviour
     /// </summary>
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "飛鏢" && other.transform.position.y > 2.4f)                               //如果碰撞物件標籤為飛鏢，且高度>2.4
-        {
-            AddScore();
-            other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;           //把飛鏢黏在面板上
-        }
+        //if (other.tag == "飛鏢" && other.transform.position.y > 2.4f)                               //如果碰撞物件標籤為飛鏢，且高度>2.4
+        //{
+        //    AddScore();
+        //    other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;           //把飛鏢黏在面板上
+        //}
         if (other.transform.root.name =="Player")
         {
             scoreIn = 500;
@@ -45,14 +52,15 @@ public class ScoreManager : MonoBehaviour
         if (other.transform.root.name =="Player")
         {
             score = 50;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         }
     }
     /// <summary>
     /// 加分
     /// </summary>
-    private void AddScore()
+    public void AddScore(int add = 1)
     {
-        score += scoreIn;
+        score += (scoreIn * add);
         textScore.text = "分數：" + score;
         aud.PlayOneShot(soundIn, Random.Range(0.5f, 1f));
     }
